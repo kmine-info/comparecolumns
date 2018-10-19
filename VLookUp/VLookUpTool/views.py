16f082e4dca8642a998c0dcb001d36c41ce6a0b2
sarath_kmine <sarath@kmine.com>
2018-10-19 21:25:24 +0000

    fix bug remove whitespace in list dataset submission on view function

--- VLookUp/VLookUpTool/views.py
+++ VLookUp/VLookUpTool/views.py
@@ -16,14 +16,12 @@ class Vlookup(FormView):
     form_class = VlookupForm
     
     def set_difference_calc(self,field1,field2):
-        print("work")
         list_index = field1 + field2
         union_result=sorted((set(field1) | set(field2)), key=list_index.index)
         intersection_result=sorted((set(field1) & set(field2)), key=list_index.index)
         differnce_A_B_result=sorted((set(field1) - set(field2)), key=list_index.index)
         difference_B_A_result=sorted((set(field2) - set(field1)), key=list_index.index)
         Symmetric_difference_result=sorted((set(field1) ^ set(field2)), key=list_index.index)
-        
         context = {
            'field1':field1,
            'field2':field2,  
@@ -35,19 +33,25 @@ class Vlookup(FormView):
            
             }
         
+        print(union_result)
+        
         return context
     
     def form_valid(self, form): 
         
-        #import ipdb; ipdb.set_trace()
-        print("work")
+        
         field1 = form.cleaned_data['lookup_field1'].splitlines()
         field2 = form.cleaned_data['lookup_field2'].splitlines()
+        
+        field1 = list(filter(None, field1))
+        field2 = list(filter(None, field2))
+        
         
         
         context = self.set_difference_calc(field1, field2)
         
         context ['form'] = form
-    
+        
+        
         return render(self.request, 'VLookUpTool/vlook_up.html', context)
     
