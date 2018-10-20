391f5fceaefcc66755634372f27d59b5e9291b05
Saravanan <saravanan@kmine.com>
2018-10-20 22:53:51 +0100

    Updated settings to support AWS Environment in ALLOWED_HOSTS automatically

--- src/VLookUp/settings.py
+++ src/VLookUp/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
+import requests
+try:
+    internal_ip = requests.get('http://instance-data/latest/meta-data/local-ipv4').text
+except requests.exceptions.ConnectionError:
+    pass
+else:
+    ALLOWED_HOSTS.append(internal_ip)
+del requests
+
 
 # Application definition
 
